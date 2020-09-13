4ef191df7d4d5d9e6a5193daccd54f59b64ae2e7
Yi Lin Lee <yilinlee19@gmail.com>
2020-09-13 17:07:53 +1000

    configure to deploy to Heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,13 +15,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
+
+app.get("/", function (req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
